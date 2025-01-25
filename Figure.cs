b883278b26d0f733b7eedf902517a47c0507dce9
Daria Usik <d_k_08@bk.ru>
2025-01-25 22:01:00 +0300

    Задание №3: Созданы вспомогательные методы и классы

---
+++ Figure.cs
@@ -0,0 +1,13 @@
+﻿namespace addressbook_web_tests
+{
+    internal class Figure
+    {
+        private bool colored = false;
+
+        public bool Colored
+        {
+            get { return colored; }
+            set { colored = value; }
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,18 @@
+﻿namespace addressbook_web_tests
+{
+    internal class Square: Figure
+    {
+        private int size;
+
+        public Square(int size)
+        {
+            this.size = size;
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+    }
+}
