30544f1d47ea1be971cf96bb9dbf3c1f7dfbab41
Daria Usik <d_k_08@bk.ru>
2025-03-11 15:37:07 +0300

    Лекция 6.1. Чтение и запись текстовых файлов

---
+++ addressbook-test-data-generators/Program.cs
@@ -0,0 +1,13 @@
+﻿using WebAddressbookTests;
+
+int count = Convert.ToInt32(args[0]);
+StreamWriter writer = new StreamWriter(args[1]);
+
+for(int i = 0; i < count; i++)
+{
+    writer.WriteLine(String.Format("${0},${1},${2}",
+        TestBase.GenerateRandomString(10), 
+        TestBase.GenerateRandomString(10), 
+        TestBase.GenerateRandomString(10)));
+}
+writer.Close();
