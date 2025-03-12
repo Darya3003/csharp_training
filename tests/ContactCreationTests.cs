9cb374980a225e6782ca22ab1d37c4089d55685a
Daria Usik <d_k_08@bk.ru>
2025-03-12 12:00:40 +0300

    Задание №15: загрузка данных из файла

--- tests/ContactCreationTests.cs
+++ tests/ContactCreationTests.cs
@@ -1,17 +1,81 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Excel = Microsoft.Office.Interop.Excel;
+
 
 namespace WebAddressbookTests
 {
     [TestFixture]
     public class ContactCreationTests : AuthTestBase
     {
-        [Test]
+        public static IEnumerable<ContactData> RandomContactDataProvider()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                contacts.Add(new ContactData(GenerateRandomString(30))
+                {
+                    LastName = GenerateRandomString(100)                    
+                });
+            }
+
+            return contacts;
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromCsvFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            string[] lines = File.ReadAllLines(@"contacts.csv");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                contacts.Add(new ContactData(parts[0])
+                {
+                    LastName = parts[1]
+                });
+            }
+            return contacts;
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromXmlFile() 
+                    =>(List<ContactData>)
+                    new XmlSerializer(typeof(List<ContactData>)).Deserialize(new StreamReader(@"contacts.xml"));
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile() 
+                   => JsonConvert.DeserializeObject<List<ContactData>>
+                   (File.ReadAllText(@"contacts.json"));
+
+        public static IEnumerable<ContactData> ContactDataFromExcelFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb = app.Workbooks.Open(Path.Combine(Directory.GetCurrentDirectory(), @"contacts.xlsx"));
+            Excel.Worksheet sheet = wb.ActiveSheet;
+            Excel.Range range = sheet.UsedRange;
+            for (int i = 1; i <= range.Rows.Count; i++)
+            {
+                contacts.Add(new ContactData()
+                {
+                    FirstName = range.Cells[i, 1].Value,
+                    LastName = range.Cells[i, 2].Value
+                });
+            }
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
+
+            return contacts;
+        }
+
+        [Test, TestCaseSource("ContactDataFromJsonFile")]
         public void ContactCreationTest()
         {
-            ContactData contactData = new ContactData("qqq")
+            ContactData contactData = new ContactData("www")
             {
-                LastName = "www"
+                LastName = "qqq"
             };
 
             if (!app.Contact.IsAnyContactExist())
